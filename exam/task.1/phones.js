222ccfe1e66fea8a8bfc4c9f24d676e3b12c4d4f
Turusbekova-Guliza <134058547+Turusbekova-Guliza@users.noreply.github.com>
2023-07-19 14:19:57 +0600

    sdfgh

--- exam/task.1/phones.js
+++ exam/task.1/phones.js
@@ -1,29 +1,39 @@
-const div = document.querySelector("#div")
-const name = document.querySelector("#name")
-const price = document.querySelector("#price")
-const ssd = document.querySelector("#ssd")
-const color = document.querySelector("#color")
-const phoneIcon = document.querySelector("#phone-icon")
-const btn = document.querySelector("#btn")
-const input = document.querySelector("#input")
+const div = document.querySelector("#div");
+const name = document.querySelector("#name");
+const price = document.querySelector("#price");
+const ssd = document.querySelector("#ssd");
+const color = document.querySelector("#color");
+const phoneIcon = document.querySelector("#phone-icon");
+const btn = document.querySelector("#btn");
+const input = document.querySelector("#input");
 
-const baseURL = "./package.json"
+const baseURL = "./package.json";
 console.log("./package.json");
 
 function fetchPhones() {
-    fetch("./package.json")
-        .then((response) => response.json())
-        .then((phoneData) => {
-            console.log(phoneData);
-            phoneCard(phoneData)
-        })
+  fetch("./package.json")
+    .then((response) => response.json())
+    .then((phoneData) => {
+      console.log(phoneData);
+    });
 }
-fetchPhones()
+fetchPhones();
+
+btn.onclick = () => {
+  if (input.value.trim().toLowerCase()) {
+    fetch("./package.json" + input.value)
+      .then((res) => res.json())
+      .then((phones) => {
+        console.log(phones);
+        phoneCard(phones)
+      });
+  }
+};
 
 function phoneCard(data) {
-    div.innerHTML = ""
-    for(let i = 0; i < data.length; i++) {
-        div.innerHTML += `
+  div.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    div.innerHTML += `
         <div class="card" style="width: 18rem;">
              <img id="phone-icon" src="${data[i].img}" alt="">
             <div>
@@ -33,10 +43,7 @@ function phoneCard(data) {
              <p id="color">Color: ${data[i].color}</p>
             </div>
         </div>
-        `
-    }
+        `;
+  }
 }
 
-btn.onclick = () => {
-
-}
